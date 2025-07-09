b54e98368d153e9a648c1908ee560e4d4f43cdf1
yassh <7814130+yassh@users.noreply.github.com>
2025-07-10 08:01:35 +0900

    update payload-types.ts

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -119,6 +119,7 @@ export interface UserAuthOperations {
  */
 export interface User {
   id: number;
+  role: 'admin' | 'user';
   updatedAt: string;
   createdAt: string;
   email: string;
@@ -218,6 +219,7 @@ export interface PayloadMigration {
  * via the `definition` "users_select".
  */
 export interface UsersSelect<T extends boolean = true> {
+  role?: T;
   updatedAt?: T;
   createdAt?: T;
   email?: T;
